1302888e0b5403fd5d37b9783aa486d6d852d61d
Jonathon Ball <jonathon.ball@gmail.com>
2018-08-25 15:28:49 -0400

    added rgb to hsv function

---
+++ index.js
@@ -0,0 +1,53 @@
+class Color {
+
+    constructor() {
+        this.red = 0;
+        this.green = 0;
+        this.blue = 0;
+        this.hue = 0;
+        this.saturation = 0;
+        this.value = 0;
+    }
+
+    rgb() {
+        return {
+            red: this.red,
+            green: this.green,
+            blue: this.blue
+        };
+    }
+
+    hsv() {
+        return {
+            hue: this.hue,
+            saturation: this.saturation,
+            value: this.value
+        };
+    }
+
+    rgbToHsv() {
+        // http://coecsl.ece.illinois.edu/ge423/spring05/group8/finalproject/hsv_writeup.pdf
+        let min = Math.min(this.red, this.green, this.blue);
+        let max = Math.max(this.red, this.green, this.blue);
+        this.value = max;
+        let delta = max - min;
+        if ( max === 0) {
+            this.saturation = 0;
+            this.hue = -1;
+            return;
+        }
+        this.saturation = delta / max;
+        if (this.red == max) {
+            this.hue = (this.green - this.blue) / delta;
+        } else if (this.green == max) {
+            this.hue = 2 + (this.blue - this.red) / delta;
+        } else {
+            this.hue = 4 + (this.red - this.green) / delta;
+        }
+        this.hue *= 60;
+        if (this.hue < 0) {
+            this.hue += 360;
+        }
+    }
+
+}
