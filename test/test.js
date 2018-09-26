11e49ef98ad6f776dd144bf9033813ddff7e6fc6
Jonathon Ball <jonathon.ball@gmail.com>
2018-09-25 22:18:54 -0400

    added some mocha tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,25 @@
 var assert = require('assert');
+var Color = require('..');
 
 describe('Color', function() {
     describe('#rgb()', function() {
-        it('should return an object', function() {
-            //assert.
+        it('should return an object with rgb related properties', function() {
+            var color = new Color();
+            assert.deepStrictEqual(color.rgb(), {
+                red: 0,
+                green: 0,
+                blue: 0
+            });
+        });
+    });
+    describe('#hsv()', function() {
+        it('should return an object with hsv related properties', function() {
+            var color = new Color();
+            assert.deepStrictEqual(color.hsv(), {
+                hue: 0,
+                saturation: 0,
+                value: 0
+            });
         });
     });
 });
